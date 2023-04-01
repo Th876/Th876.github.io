1bbab28be88cda5bcadd3b942626f903c84fdf5e
TH876 <tayhiggins14@gmail.com>
2023-04-01 16:20:51 -0400

    had issues with git. Finally updating and adding new files/folders

--- main.js
+++ main.js
@@ -11,191 +11,385 @@ let startContainer = document.querySelector("#start-container");
 let quizContainer = document.querySelector("#quiz-container");
 quizContainer.style.visibility = "hidden";
 
+//Select background video
+let videoBkground = document.querySelector(".vid-background");
+
 //Select name input field
 const inputName = document.getElementById("input-name");
 
 //Test validation for name input field via Start Quiz button status
-inputName.addEventListener("input", function(e){
-    let regex = /^[A-Za-z]+$/; //matching letters for a first name only
-    
-    // If name matches regex, button is enabled and user can start game
-    if(inputName.value.match(regex)){
-        document.querySelector(".startBtn").disabled = false;
-        //call a function here that makes button load to new page?
-    } 
-    else {
-        //Button is disabled 
-        document.querySelector(".startBtn").disabled = true;
-    }
+inputName.addEventListener("input", function (e) {
+  //matching letters for a first name with or without hyphen & no space
+  let regex = /^[a-zA-Z]+(-[a-zA-Z]+)?$/;
+
+  // If name matches regex, button is enabled and user can start game
+  if (inputName.value.match(regex)) {
+    document.querySelector(".startBtn").disabled = false;
+  } else {
+    //Button is disabled
+    document.querySelector(".startBtn").disabled = true;
+  }
 });
 
-//Display Quiz page when Start game button is clicked 
-startBtn.addEventListener("click", function(e){
-    startContainer.remove();
-    quizContainer.style.visibility = "visible";
+//Display Quiz page when Start game button is clicked
+startBtn.addEventListener("click", function (e) {
+  startContainer.style.visibility = "hidden";
+  quizContainer.style.visibility = "visible";
+  videoBkground.style.visibility = "hidden";
 });
-
 
 /*-------------------------Quiz Page-------------------------*/
 
 //Get input (user's name) and display on quiz game page
-startBtn.addEventListener("click", function(e){ 
-    if(inputName.value !== " ") {
-        let name = document.querySelector(".player1")
-        name.textContent = "Player 1: " + inputName.value;
-        // console.log(playerNames);
-    }
-    generateQuestions();
+startBtn.addEventListener("click", function (e) {
+  if (inputName.value !== " ") {
+    let name = document.querySelector(".playername");
+    name.textContent = "Player: " + inputName.value;
+  }
+  generateQuestions();
+  quizMusic();
 });
 
 // Questions & answers
-
-//Store questions and answers; and validate answers - TESTING WITH 6 QUESTIONS FIRST
+//Store questions and answers; and validate answers
 let quiz = [
+  // Round One starts
+  {
+    question: "The code in The Matrix comes from what food recipes?",
 
-    // Round One starts
-    {
-        question: "The code in The Matrix comes from what food recipes?",
-        
-        optionA: "Sushi",
-        optionB: "Stir-fry", 
-        answer: "option-a",
-},
+    optionA: "Sushi",
+    optionB: "Stir-fry",
+    answer: "option-a",
+  },
 
-{
+  {
     question: "Where were The Lord of the Rings movies filmed?",
 
     optionA: "Iceland",
     optionB: "New Zealand",
     answer: "option-b",
-},
+  },
 
-{
+  {
     question: "Freddy Krueger wears a striped sweater that is which colors?",
-    
-    optionA: "Red and green",
-    optionB: "Red and blue", 
-    answer: "option-a",
-},
 
-{
+    optionA: "Red and green",
+    optionB: "Red and blue",
+    answer: "option-a",
+  },
+
+  {
     question: "Which of these two Marvel movies was released first?",
 
     optionA: "Iron Man",
-    optionB: "Captain America: The First Avenger", 
+    optionB: "Captain America: The First Avenger",
     answer: "option-b",
-},
+  },
 
-{
-    question: "What is the name of the camp where counselors are terrorized by a slasher in Friday the 13th?",
+  {
+    question:
+      "What is the name of the camp where counselors are terrorized by a slasher in Friday the 13th?",
 
-    optionA: "Camp Holland Lake", 
-    optionB: "Camp Crystal Lake", 
+    optionA: "Camp Holland Lake",
+    optionB: "Camp Crystal Lake",
     answer: "option-b",
-},
+  },
 
-{
-    question: "Which singer starred alongside Steve Martin in 2006’s remake of The Pink Panther?",
+  {
+    question:
+      "Which singer starred alongside Steve Martin in 2006’s remake of The Pink Panther?",
 
-    optionA: "Beyoncé", 
+    optionA: "Beyoncé",
     optionB: "Britney Spears",
     answer: "option-a",
-},
+  },
 
-// Round Two starts
+  // Round Two starts
 
-//Round Three starts
+  {
+    question: "In Mean Girls, Cady moves to Illinois from which continent?",
+
+    optionA: "Africa ",
+    optionB: "Asia",
+    answer: "option-a",
+  },
+
+  {
+    question:
+      "Which professional athlete was considered for the lead in The Terminator?",
+
+    optionA: "Mike Tyson",
+    optionB: "O.J. Simpson",
+    answer: "option-b",
+  },
+
+  {
+    question: "Which Star Wars characters appear in Indiana Jones?",
+
+    optionA: "R2-D2 and C-3PO",
+    optionB: "Luke Skywalker and Princess Leia",
+    answer: "option-a",
+  },
+
+  {
+    question:
+      "In The Ring, how long do people have to live after they watch a cursed videotape?",
+
+    optionA: "Seven days",
+    optionB: "Five days",
+    answer: "option-a",
+  },
+
+  {
+    question: "What was Tyler Perry’s first movie as Madea?",
+
+    optionA: "Diary of a Mad Black Woman",
+    optionB: "Madea Goes to Jail",
+    answer: "option-a",
+  },
+
+  {
+    question: "What is the name of Jessie’s horse in Toy Story 2?",
+
+    optionA: "Gallop",
+    optionB: "Bullseye",
+    answer: "option-b",
+  },
+
+  //Round Three starts
+  {
+    question: "What item is in every Fight Club scene?",
+
+    optionA: "Coca-Cola",
+    optionB: "Starbucks cup",
+    answer: "option-b",
+  },
+
+  {
+    question: "Which actor hasn’t played Spider-Man?",
+
+    optionA: "Tobey Maguire",
+    optionB: "Topher Grace",
+    answer: "option-b",
+  },
+
+  {
+    question:
+      "What word completes this famous quote from Jerry McGuire: “Show me the ____.",
+
+    optionA: "Proof",
+    optionB: "Money",
+    answer: "option-b",
+  },
+
+  {
+    question: "Rocky famously runs up the steps of which building in Rocky? ",
+
+    optionA: "The Philadelphia Museum of Art",
+    optionB: "Philadelphia City Hall",
+    answer: "option-a",
+  },
+
+  {
+    question:
+      "Which two actors starred in the 2012 movie adaptation of 21 Jump Street?",
+
+    optionA: "Seth Rogen and James Franco",
+    optionB: "Jonah Hill and Channing Tatum",
+    answer: "option-b",
+  },
+
+  {
+    question:
+      "Which country does Forrest Gump travel to as part of the All-American Ping-Pong Team?",
+
+    optionA: "China",
+    optionB: "Vietnam",
+    answer: "option-a",
+  },
+  //
 ];
 
-//Set counter for question and points
-const lastQuestion = quiz.length - 1;
-let questionNow = 0;
-let points = 0;
+//Set background music variable
+let backgroundMusic = new Audio();
 
-//Select space for questions, answer options and points to generate 
+//Create play background music function
+function quizMusic() {
+  backgroundMusic.src = "/Th876.github.io/audio/gameBackgroundSound.mp3";
+  backgroundMusic.play();
+  backgroundMusic.loop = true;
+  backgroundMusic.controls = true;
+};
+
+//Create stop background music function
+function pauseMusic() { 
+    backgroundMusic.pause();
+    backgroundMusic.currentTime = 0;
+};
+
+//Generates round titles
+startBtn.addEventListener("click", function (e) {
+  if (questionNow <= 7) {
+    roundTitle.textContent = "Round One";
+    console.log("Round One");
+  } else if (questionNow <= 14) {
+    roundTitle.textContent = "Round Two";
+    console.log("Round Two");
+  } else {
+    roundTitle.textContent = "Round Three";
+    console.log("Round Three");
+  }
+});
+
+//Select space for questions, answer options and points to generate
 const quesContainer = document.querySelector("#ques-container");
 const quizQuestions = document.querySelector("#quiz-questions");
 quesContainer.appendChild(quizQuestions);
-const optionA = document.getElementById("option-a");
-const optionB = document.getElementById("option-b");
-let humanPoints = document.getElementById("human-points");
-let humanPlayerPoints = document.querySelector("#human-player-points"); 
-let humanTotalPoints = document.querySelector("#human-total-points");
+const optionA = document.querySelector("#option-a");
+const optionB = document.querySelector("#option-b");
+let userPoints = document.querySelector("#user-points");
+let playerPointsEarned = document.querySelector("#player-points-earned"); 
+let totalPoints = document.querySelector("#total-points");
+let roundTitle = document.querySelector(".round-title");
+const exitGame = document.querySelector(".end-btn");
+const quizBtn = document.querySelector(".quiz-btn");
+
+
+//Set counter for question and points
+const lastQuestion = quiz.length - 1;
+let questionNow = 0; //current question
+let points = 0; //user points earned
 
 //Create function to generate questions and answers
 function generateQuestions() {
-    //Create audio element
-    // let backgroundMusic = new Audio();
-    // backgroundMusic.src = " .mp3";
-    // backgroundMusic.play();
+  //Updates points received out of the questions that exist
+  totalPoints.textContent = quiz.length;
 
-    //Updates points received out of the questions that exist
-    humanTotalPoints.textContent = quiz.length;
+  //Generate each question in the array at this position on the page and set its text content
+  quizQuestions.textContent = quiz[questionNow].question;
 
-    //Generate each question in the array at this position on the page and set its text content
-    quizQuestions.textContent = quiz[questionNow].question;
-    
-    //Set the text content of every first option (optionA)
-    optionA.textContent = quiz[questionNow].optionA;
+  //Set the text content of every first option (optionA)
+  optionA.textContent = quiz[questionNow].optionA;
 
-     //Set the text content of every second option (optionB)
-     optionB.textContent = quiz[questionNow].optionB;
+  //Set the text content of every second option (optionB)
+  optionB.textContent = quiz[questionNow].optionB;
 }
 
- generateQuestions();
+generateQuestions();
+
+//Create function to validate answers
+optionA.addEventListener("click", function (e) {
+  //if user clicks the first option and it is correct, show green border
+  if (quiz[questionNow].answer === "option-a") {
+    //increment points
+    points++;
+
+    //increments user's points earned
+    console.log(userPoints.children);
+    userPoints.children[0].textContent = points;
+    console.log(points);
+    optionA.style.backgroundColor = "var(--green)";
+    optionA.style.color = "var(--white)";
+    rightAnswer();
+  }
+  //if user clicks the first option and it is correct, show red border
+  else {
+    optionA.style.backgroundColor = "var(--red)";
+    optionA.style.color = "var(--white)";
+    wrongAnswer();
+  }
+
+  //Generate next question
+  if (questionNow < quiz.length) {
+    questionNow++;
+
+    checkRound();
+
+    //Delay next set of questions & answers; reset border to black
+    setTimeout(() => {
+      optionA.style.backgroundColor = "transparent";
+      optionA.style.color = "var(--black)";
+      generateQuestions();
+    }, 1000);
+  }
+});
+
+optionB.addEventListener("click", function (e) {
+  //if user clicks the first option and it is correct, show green border
+  if (quiz[questionNow].answer === "option-b") {
+    //increment points
+    points++;
+    userPoints.children[0].textContent = points;
+    optionB.style.backgroundColor = "var(--green)";
+    optionB.style.color = "var(--white)";
+    rightAnswer();
+  }
+  //if user clicks the first option and it is correct, show red border
+  else {
+    optionB.style.backgroundColor = "var(--red)";
+    optionB.style.color = "var(--white)";
+    wrongAnswer();
+  }
+
+  //Generate next question
+  if (questionNow < quiz.length) {
+    questionNow++;
+
+    checkRound();
+
+    //Delay next set of questions & answers; reset border to black
+    setTimeout(() => {
+      optionB.style.backgroundColor = "transparent";
+      optionB.style.color = "var(--black)";
+      generateQuestions();
+    }, 1000);
+  }
+});
+
+//Create function to display round titles
+function checkRound() {
+    setTimeout(() => {
+  if (questionNow == 0) {
+    roundTitle.textContent = "Round One";
+    console.log("Round One");
+  } else if (questionNow == 6) {
+    roundTitle.textContent = "Round Two";
+    roundTitle.style.color = "var(--dusk)";
+    console.log("Round Two");
+  } else if (questionNow == 12) {
+    roundTitle.textContent = "Round Three";
+    roundTitle.style.color = "var(--orangesicle)";
+    console.log("Round Three");
+  }
+} , 1000);
+}
+
+// Add sound to right answers chosen
+function rightAnswer() {
+  let right = new Audio();
+  right.src = "/Th876.github.io/audio/correct-point.mp3";
+  right.play();
+}
+
+// Add sound to wrong answers chosen
+function wrongAnswer() {
+  let wrong = new Audio();
+  wrong.src = "/Th876.github.io/audio/wrong-point.mp3";
+  wrong.play();
+}
 
 
-       //Create function to validate answers
-    optionA.addEventListener("click", function(e) {
-    //if user clicks the first option and it is correct, show green border
-        if(quiz[questionNow].answer === "option-a") {
-            //increment points
-            points++;
+//Create a function to show quiz page
+function hideQuizPage() {
+  quizContainer.style.visibility = "hidden";
+}
 
-            // console.log(humanPoints.children); testing to see its children
-            humanPoints.children[0].textContent = points;
-            // console.log(points); testing to see if its incrementing
-            optionA.style.border = "2px solid var(--green)";
-        }  
-        //if user clicks the first option and it is correct, show red border
-        else {
-            optionA.style.border = "2px solid var(--red)"; 
-        }
-
-
-       //Generate next question
-       if(questionNow < lastQuestion) {
-        questionNow++; 
-        
-        //Delay next set of questions & answers; reset border to black
-        setTimeout(() => {
-        optionA.style.border = "1px solid var(--black)";
-        generateQuestions();
-        } , 1000);
-       }
-        });
-
-        optionB.addEventListener("click", function(e) {
-            //if user clicks the first option and it is correct, show green border
-                if(quiz[questionNow].answer === "option-b") {
-                     //increment points
-                    points++;
-                    humanPoints.children[0].textContent = points;
-                    optionB.style.border = "2px solid var(--green)"; 
-               } 
-               //if user clicks the first option and it is correct, show red border
-               else {
-                optionB.style.border = "2px solid var(--red)"; 
-               }
-
-                //Generate next question
-               if(questionNow < lastQuestion) {
-                questionNow++;
-             
-                //Delay next set of questions & answers; reset border to black
-                setTimeout(() => {
-                    optionB.style.border = "1px solid var(--black)";
-                    generateQuestions();
-                    } , 1000);
-               }
-                });
+//Event listener that leaves the quiz page and goes to the start page
+exitGame.addEventListener("click", function (e) {
+  startContainer.style.visibility = "visible";
+  videoBkground.style.visibility = "visible";
+  inputName.value = "";
+  startBtn.disabled = true;
+  hideQuizPage();
+  pauseMusic();
+});
